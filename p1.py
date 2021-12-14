46485b3cc10faca5644984c9881cb2a8f06f5242
Abel Asfaw <abel.f.asfaw@gmail.com>
2021-12-13 23:43:34 -0500

    first commit

---
+++ p1.py
@@ -0,0 +1,8 @@
+inputs = [1.2,5.1,2.1]
+# evey input has a weights
+weights = [3.1, 2.1, 8.7]
+# every neuroan has a bias
+bias = 3
+
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+print(output)
