f784de5ee82d7209d6ce0253a40f9728165fd16a
AbelNyc <53309012+AbelNyc@users.noreply.github.com>
2021-12-15 22:21:49 -0500

    Add files via upload

--- p1.py
+++ p1.py
@@ -1,8 +1,16 @@
-inputs = [1.2,5.1,2.1]
+inputs = [1.2,5.1,2.1,3.5]
 # evey input has a weights
-weights = [3.1, 2.1, 8.7]
-# every neuroan has a bias
-bias = 3
+weights1 = [3.1, 2.1, -8.7, 2.3]
+weights2 = [3.2, -1.5, 8.5, -2.2]
+weights3 = [2.9, -2.5, 8.1, 2.1]
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
-print(output)
+# every neuroan has a bias
+bias1, bias2, bias3 = 2,3,.5
+
+
+# inputs layer: input * weight + bias 
+output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
+          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
+          inputs[0] * weights1[0] + inputs[1] * weights2[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3,
+]
+print("Three neurons:",output)
